c51e83df4ee8a7b60d26ff525e4e112de42ef705
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-04-24 13:24:37 -0300

    Break the app on purpose

--- cypress/integration/Teste Autovist.spec.js
+++ cypress/integration/Teste Autovist.spec.js
@@ -11,8 +11,8 @@ describe('Teste Sistema Autovist', function() {
     it('preenche os campos e faz login', function(){
         cy.get('#id_email').type(Cypress.env("user_name"))
         cy.get('#id_senha').type(Cypress.env("user_password"))
-        cy.wait(3000)
-        cy.get('#logar').click()
+        cy.wait(2000)
+        cy.get('#logar').click()   
     })
   })
   
